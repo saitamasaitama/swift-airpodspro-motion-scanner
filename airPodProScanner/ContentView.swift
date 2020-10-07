6f22e3394830983cb2573f4a1fa772bd90308d06
saitamasaitama <saitamasaitama@saitama-tec.co.jp>
2020-10-07 23:39:01 +0900

    Initial Commit

---
+++ airPodProScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  airPodProScanner
+//
+//  Created by shinp on 2020/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ airPodProScanner/airPodProScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  airPodProScannerApp.swift
+//  airPodProScanner
+//
+//  Created by shinp on 2020/10/07.
+//
+
+import SwiftUI
+
+@main
+struct airPodProScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
